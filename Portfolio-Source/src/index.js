e9ab017d31c0fc29e435501abff43f785eae58e4
PrafullaS <112618564+PrafullaS@users.noreply.github.com>
2025-02-03 23:18:29 +0530

    Update index.js

--- Portfolio-Source/src/index.js
+++ Portfolio-Source/src/index.js
@@ -1,29 +1,17 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App"; // Make sure App.js exists in src folder
-import "./index.css"; // Ensure this file exists and styles are applied
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+reportWebVitals();
